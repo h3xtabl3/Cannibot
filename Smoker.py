d0b865d0c36dc6f51ef740d629e403108da0a751
Nikki Hux <Nikki@Nikkis-MacBook-Pro.local>
2017-10-20 12:34:53 -0700

    Initial commit

---
+++ Smoker.py
@@ -0,0 +1,11 @@
+class Smoker:
+
+    def __init__Smoker(self):
+
+        self.name                   #name of smoker
+        self.tools                  #tools smoker needs to smoke
+        self.preferences            #papers or blunts
+        self.money                  #amount of money smoker has
+        self.stash                  #all the weed the smoker has
+
+
---
+++ Strain.py
@@ -0,0 +1,8 @@
+class Strain:
+
+    def __init__Strain(self):
+
+        self.name                       #name of strain
+        self.type                       #type of strain (indica sativa or hybrid)
+        self.sideEffect                 #how do you feel when you smoke it?
+        self.potency                    #thc and cbd levels
